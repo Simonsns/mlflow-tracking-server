a34044578485fb853ddfede62b625d276a885d38
Simon.sngs <simon.sngs@hotmail.com>
2026-04-08 19:51:00 +0200

    fix: RAM usage too high

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,6 @@ exec /opt/venv/bin/mlflow server \
     --artifacts-destination ${MLFLOW_S3_BUCKET} \
     --app-name basic-auth \
     --allowed-hosts ${RAILWAY_PUBLIC_DOMAIN} \
-    --cors-allowed-origins ${RAILWAY_PUBLIC_DOMAIN} \
+    --cors-allowed-origins "*" \
     --workers 1 \
     --uvicorn-opts "--limit-concurrency 20"
